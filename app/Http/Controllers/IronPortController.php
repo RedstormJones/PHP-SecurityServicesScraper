01d403f144452342bbc64883a05b3b2873645b1e
RedstormJones <tfilkins@unomaha.edu>
2016-10-12 12:35:27 +0000

    Applied fixes from StyleCI

--- app/Http/Controllers/IronPortController.php
+++ app/Http/Controllers/IronPortController.php
@@ -2,29 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-
 class IronPortController extends Controller
 {
-	/**
-	* Create new IronPort Controller instance
-	*
-	* @return void
-	*/
-	public function __constuct()
-	{
-		$this->middleware('auth');
-	}
+    /**
+     * Create new IronPort Controller instance.
+     *
+     * @return void
+     */
+    public function __constuct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	* Show the IronPort dashboard
-	*
-	* @return \Illuminate\Http\Response
-	*/
-	public function index()
-	{
-		return view('IronPort');
-	}
+    /**
+     * Show the IronPort dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('IronPort');
+    }
 }
--- app/Http/Controllers/SecurityCenterController.php
+++ app/Http/Controllers/SecurityCenterController.php
@@ -2,29 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-
 class SecurityCenterController extends Controller
 {
-	/**
-	* Create new SecurityCenter Controller instance
-	*
-	* @return void
-	*/
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    /**
+     * Create new SecurityCenter Controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	* Show SecurityCenter dashboard
-	*
-	* @return \Illuminate\Http\Response
-	*/
-	public function index()
-	{
-		return view('SecurityCenter');
-	}
+    /**
+     * Show SecurityCenter dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('SecurityCenter');
+    }
 }
