545cc323df2261871ba376390de0d8fd7661de8f
RedstormJones <tyler.filkins@kiewit.com>
2016-10-25 22:59:22 +0000

    modified scheduled commands to run on CST timezone instead of UTC

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -51,43 +51,43 @@ class Kernel extends ConsoleKernel
         /*
         * Commands run on monthly schedule
         */
-        $schedule->command('crawl:ironportthreats')->monthlyOn(1, '21:00');     // runs every month on the 1st at 09:00pm
-        $schedule->command('process:ironportthreats')->monthlyOn(1, '21:30');   // runs every month on the 1st at 09:30pm
-        $schedule->command('crawl:phishmescenarios')->monthlyOn(1, '22:30');    // runs every month on the 1st at 10:30pm
-        $schedule->command('process:phishmescenarios')->monthlyOn(1, '23:00');  // runs every month on the 1st at 11:00pm
+        $schedule->command('crawl:ironportthreats')->monthlyOn(1, '21:00')->timezone('America/Chicago');     // runs every month on the 1st at 09:00pm
+        $schedule->command('process:ironportthreats')->monthlyOn(1, '21:30')->timezone('America/Chicago');   // runs every month on the 1st at 09:30pm
+        $schedule->command('crawl:phishmescenarios')->monthlyOn(1, '22:30')->timezone('America/Chicago');    // runs every month on the 1st at 10:30pm
+        $schedule->command('process:phishmescenarios')->monthlyOn(1, '23:00')->timezone('America/Chicago');  // runs every month on the 1st at 11:00pm
 
         /*
         * Commands run on weekly schedule
         */
-        $schedule->command('crawl:securitycenterassetvulns')->weekly()->thursdays()->at('17:30');   // runs weekly on Thursdays at 5:30pm
-        $schedule->command('process:securitycenterassetvulns')->weekly()->thursdays()->at('17:45'); // runs weekly on Thursdays at 5:45pm
-        $schedule->command('crawl:securitycentervulns')->weekly()->thursdays()->at('18:00');        // runs weekly on Thursdays at 6:00pm
-        $schedule->command('process:securitycentercriticals')->weekly()->thursdays()->at('18:15');  // runs weekly on Thursdays at 6:15pm
-        $schedule->command('process:securitycenterhighs')->weekly()->thursdays()->at('19:30');      // runs weekly on Thursdays at 7:30pm
-        $schedule->command('process:securitycentermediums')->weekly()->thursdays()->at('21:00');    // runs weekly on Thursdays at 9:00pm
+        $schedule->command('crawl:securitycenterassetvulns')->weekly()->thursdays()->at('17:30')->timezone('America/Chicago');   // runs weekly on Thursdays at 5:30pm
+        $schedule->command('process:securitycenterassetvulns')->weekly()->thursdays()->at('17:45')->timezone('America/Chicago'); // runs weekly on Thursdays at 5:45pm
+        $schedule->command('crawl:securitycentervulns')->weekly()->thursdays()->at('18:00')->timezone('America/Chicago');        // runs weekly on Thursdays at 6:00pm
+        $schedule->command('process:securitycentercriticals')->weekly()->thursdays()->at('18:15')->timezone('America/Chicago');  // runs weekly on Thursdays at 6:15pm
+        $schedule->command('process:securitycenterhighs')->weekly()->thursdays()->at('19:30')->timezone('America/Chicago');      // runs weekly on Thursdays at 7:30pm
+        $schedule->command('process:securitycentermediums')->weekly()->thursdays()->at('21:00')->timezone('America/Chicago');    // runs weekly on Thursdays at 9:00pm
 
-        $schedule->command('crawl:sitesubnets')->weekly()->fridays()->at('22:00');      // runs weekly on Fridays at 10:00pm
-        $schedule->command('process:sitesubnets')->weekly()->fridays()->at('22:15');    // runs weekly on Fridays at 10:15pm
+        $schedule->command('crawl:sitesubnets')->weekly()->fridays()->at('22:00')->timezone('America/Chicago');      // runs weekly on Fridays at 10:00pm
+        $schedule->command('process:sitesubnets')->weekly()->fridays()->at('22:15')->timezone('America/Chicago');    // runs weekly on Fridays at 10:15pm
 
         /*
         * Commands run on daily schedule
         */
-        $schedule->command('crawl:spamemails')->dailyAt('10:30');
-        $schedule->command('process:spamemail')->dailyAt('11:00');
-        $schedule->command('crawl:cylancedevices')->daily();                    // runs at midnight (00:00)
-        $schedule->command('process:cylancedevices')->dailyAt('01:00');
-        $schedule->command('crawl:cylancethreats')->dailyAt('03:30');
-        $schedule->command('process:cylancethreats')->dailyAt('04:00');
-        $schedule->command('crawl:incomingemail')->dailyAt('05:00');
-        $schedule->command('process:incomingemail')->dailyAt('05:15');
+        $schedule->command('crawl:spamemails')->dailyAt('10:30')->timezone('America/Chicago');
+        $schedule->command('process:spamemail')->dailyAt('11:00')->timezone('America/Chicago');
+        $schedule->command('crawl:cylancedevices')->daily()->timezone('America/Chicago');                    // runs at midnight (00:00)
+        $schedule->command('process:cylancedevices')->dailyAt('01:00')->timezone('America/Chicago');
+        $schedule->command('crawl:cylancethreats')->dailyAt('03:30')->timezone('America/Chicago');
+        $schedule->command('process:cylancethreats')->dailyAt('04:00')->timezone('America/Chicago');
+        $schedule->command('crawl:incomingemail')->dailyAt('05:00')->timezone('America/Chicago');
+        $schedule->command('process:incomingemail')->dailyAt('05:15')->timezone('America/Chicago');
 
         /*
         * Commands run multiple times a day
         */
-        $schedule->command('crawl:insidehosttrafficsnapshots')->twiceDaily(6, 12);    // runs twice every day at 6:00am and 12:00pm
-        $schedule->command('process:insidehosttrafficsnapshots')->twiceDaily(7, 13);  // runs twice every day at 7:00am and 1:00pm
-        $schedule->command('crawl:outsidehosttrafficsnapshots')->twiceDaily(8, 14);   // runs twice every day at 8:00am and 2:00pm
-        $schedule->command('process:outsidehosttrafficsnapshots')->twiceDaily(9, 15); // runs twice every day at 9:00am and 3:00pm
+        $schedule->command('crawl:insidehosttrafficsnapshots')->twiceDaily(6, 12)->timezone('America/Chicago');    // runs twice every day at 6:00am and 12:00pm
+        $schedule->command('process:insidehosttrafficsnapshots')->twiceDaily(7, 13)->timezone('America/Chicago');  // runs twice every day at 7:00am and 1:00pm
+        $schedule->command('crawl:outsidehosttrafficsnapshots')->twiceDaily(8, 14)->timezone('America/Chicago');   // runs twice every day at 8:00am and 2:00pm
+        $schedule->command('process:outsidehosttrafficsnapshots')->twiceDaily(9, 15)->timezone('America/Chicago'); // runs twice every day at 9:00am and 3:00pm
     }
 
     /**
