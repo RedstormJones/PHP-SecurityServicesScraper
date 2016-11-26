140ea3b40e32b4907e269b25344a027874a5ce8f
RedstormJones <tyler.filkins@kiewit.com>
2016-11-26 04:36:26 +0000

    updates to ServiceNow Controller

--- app/Http/Controllers/ServiceNowController.php
+++ app/Http/Controllers/ServiceNowController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\ServiceNow\cmdbServer;
+use App\ServiceNow\ServiceNowIdmIncident;
+use App\ServiceNow\ServiceNowIncident;
+use App\ServiceNow\ServiceNowSapRoleAuthIncident;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
 class ServiceNowController extends Controller
 {
     //
