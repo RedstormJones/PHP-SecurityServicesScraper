91b7d534263ce95f42541e5abf9d51d601acf4f6
RedstormJones <tfilkins@unomaha.edu>
2016-11-27 22:36:37 +0000

    Apply fixes from StyleCI

--- routes/api.netman.php
+++ routes/api.netman.php
@@ -1,32 +1,26 @@
 <?php
 
 $options = [
-	'prefix'		=> 'netman',
-	'namespace'		=> 'App\Http\Controllers',
-	'middleware'	=> [
-		'api.auth',
-		'api.throttle',
-	],
-	'limit'			=> 100,
-	'expires'		=> 5,
+    'prefix'           => 'netman',
+    'namespace'        => 'App\Http\Controllers',
+    'middleware'       => [
+        'api.auth',
+        'api.throttle',
+    ],
+    'limit'            => 100,
+    'expires'          => 5,
 ];
 
 
 
 $api->group($options, function ($api) {
+    $api->group(['prefix' => 'site_subnets'], function ($api) {
+        $api->get('/list', 'NetmanSiteSubnetController@listAll');
 
-	$api->group(['prefix' => 'site_subnets'], function ($api) {
+        $api->get('/ip/{ip}', 'NetmanSiteSubnetController@getSiteSubnetByIP');
 
-		$api->get('/list', 'NetmanSiteSubnetController@listAll');
+        $api->get('/netmask/{netmask}', 'NetmanSiteSubnetController@getSiteSubnetsByNetmask');
 
-		$api->get('/ip/{ip}', 'NetmanSiteSubnetController@getSiteSubnetByIP');
-
-		$api->get('/netmask/{netmask}', 'NetmanSiteSubnetController@getSiteSubnetsByNetmask');
-
-		$api->get('/site/{site}', 'NetmanSiteSubnetController@getSiteSubnetsBySite');
-	});
-
-
-
-
+        $api->get('/site/{site}', 'NetmanSiteSubnetController@getSiteSubnetsBySite');
+    });
 });
