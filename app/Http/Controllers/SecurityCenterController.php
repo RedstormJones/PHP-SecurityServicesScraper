eb2b10bb82333bd39c82e1d4fa3663c164c2caca
RedstormJones <tyler.filkins@kiewit.com>
2016-12-08 04:00:05 +0000

    Progress on SecurityCenter API endpoints

--- app/Http/Controllers/SecurityCenterController.php
+++ app/Http/Controllers/SecurityCenterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\SecurityCenter\SecurityCenterAssetVuln;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
+
 class SecurityCenterController extends Controller
 {
     /**
@@ -18,7 +19,7 @@ class SecurityCenterController extends Controller
     }
 
     /**
-     * Get all SecurityCenter asset vulnerabilities.
+     * Get all SecurityCenter asset vulnerabilities
      *
      * @return \Illuminate\Http\Response
      */
@@ -28,18 +29,24 @@ class SecurityCenterController extends Controller
 
         try {
             $data = [];
+            $regex = '/([A-Z][A-Z][A-Z] - .+)/';
 
-            $asset_vulns = SecurityCenterAssetVuln::all();
+            $asset_vulns = SecurityCenterAssetVuln::orderBy('asset_name', 'asc')->get();
 
-            foreach ($asset_vulns as $asset) {
-                $data[] = \Metaclassing\Utility::decodeJson($asset['data']);
+            foreach($asset_vulns as $asset)
+            {
+                if(preg_match($regex, $asset['asset_name'])) {
+                    $data[] = \Metaclassing\Utility::decodeJson($asset['data']);
+                }
             }
 
             $response = [
                 'success'       => true,
+                'count'         => count($data),
                 'asset_vulns'   => $data,
             ];
-        } catch (\Exception $e) {
+        }
+        catch (\Exception $e) {
             $response = [
                 'success'   => false,
                 'message'   => 'Failed to get SecurityCenter asset vulnerabilities.',
@@ -48,4 +55,73 @@ class SecurityCenterController extends Controller
 
         return response()->json($response);
     }
+
+    /**
+     * Get SecurityCenter asset vulnerabilities by asset score
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAssetVulnsByScore()
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+
+        try {
+            $data = [];
+            $regex = '/([A-Z][A-Z][A-Z] - .+)/';
+
+            $asset_vulns = SecurityCenterAssetVuln::orderBy('asset_score', 'desc')->get();
+
+            foreach($asset_vulns as $asset)
+            {
+                if(preg_match($regex, $asset['asset_name'])) {
+                    $data[] = \Metaclassing\Utility::decodeJson($asset['data']);
+                }
+            }
+
+            $response = [
+                'success'       => true,
+                'count'         => count($data),
+                'asset_vulns'   => $data,
+            ];
+        }
+        catch (\Exception $e) {
+            $response = [
+                'success'   => false,
+                'message'   => 'Failed to get asset vulnerabilities.',
+            ];
+        }
+
+        return response()->json($response);
+    }
+
+
+    /**
+     * Get SecurityCenter asset vulnerabilities by asset name
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAssetVulnsByName($asset)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+
+        try {
+            $asset_vuln = SecurityCenterAssetVuln::where('asset_name', 'like', $asset.'%')->firstOrFail();
+
+            $data = \Metaclassing\Utility::decodeJson($asset_vuln['data']);
+
+            $response = [
+                'success'       => true,
+                'asset_vulns'   => $data,
+            ];
+        }
+        catch (\Exception $e) {
+            $response = [
+                'success'   => false,
+                'message'   => 'Failed to get asset vulnerabilities for asset: '.$asset,
+            ];
+        }
+
+        return response()->json($response);
+    }
+
 }
--- routes/api.securitycenter.php
+++ routes/api.securitycenter.php
@@ -11,12 +11,22 @@ $options = [
     'expires'          => 5,
 ];
 
+
 $api->group($options, function ($api) {
 
-    // SecurityCenter asset vulnerabilities route group
-    $api->group(['prefix' => 'asset_vulns'], function ($api) {
+	// SecurityCenter asset vulnerabilities route group
+	$api->group(['prefix' => 'asset_vulns'], function ($api) {
 
-        // Get all asset vulnerabilities
-        $api->get('/all_assets', 'SecurityCenterController@getAllAssetVulns');
-    });
+		// Get all asset vulnerabilities
+		$api->get('/all_assets', 'SecurityCenterController@getAllAssetVulns');
+
+		// Get asset vulnerabilities by asset score
+		$api->get('/all_assets_by_score', 'SecurityCenterController@getAssetVulnsByScore');
+
+		// Get asset vulnerabilities by asset name
+		$api->get('/asset/{asset}', 'SecurityCenterController@getAssetVulnsByName');
+
+	});
+
+
 });
