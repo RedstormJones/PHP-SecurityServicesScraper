865b9b6854283e8bbb2026610dd395d86baff68c
RedstormJones <tfilkins@unomaha.edu>
2017-01-11 15:21:38 +0000

    implemented crawler and processor for SAP role auth tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -95,6 +95,7 @@ class Kernel extends ConsoleKernel
         */
         $schedule->command('get:securitytasks')->dailyAt('19:00')->timezone('America/Chicago');         // runs daily at 07:00pm
         $schedule->command('get:idmtasks')->dailyAt('19:30')->timezone('America/Chicago');              // runs daily at 07:30pm
+        $schedule->command('get:saproleauthtasks')->dailyAt('20:00')->timezone('America/Chicago');      // runs daily at 08:00pm
 
         $schedule->command('get:cmdbservers')->dailyAt('20:30')->timezone('America/Chicago');           // runs daily at 08:30pm
         $schedule->command('get:saproleauthincidents')->dailyAt('21:00')->timezone('America/Chicago');  // runs daily at 09:00pm
