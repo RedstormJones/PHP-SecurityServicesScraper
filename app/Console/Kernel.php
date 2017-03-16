0037b6103f454165e809b60c27b858e7a350df14
RedstormJones <tfilkins@unomaha.edu>
2017-03-16 19:55:28 +0000

    re-enabled scheduled Cylance threats and IronPort spam commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -112,16 +112,14 @@ class Kernel extends ConsoleKernel
         $schedule->command('get:cylancedevices')->twiceDaily(12, 14)->timezone('America/Chicago');          // runs twice daily at 12:00pm and 02:00pm
         $schedule->command('get:cylancedevices')->twiceDaily(16, 18)->timezone('America/Chicago');          // runs twice daily at 04:00pm and 06:00pm
 
-        /*
         $schedule->command('get:cylancethreats')->twiceDaily(7, 9)->timezone('America/Chicago');            // runs twice daily at 07:00am and 09:00am
         $schedule->command('get:cylancethreats')->twiceDaily(11, 13)->timezone('America/Chicago');          // runs twice daily at 11:00am and 01:00pm
         $schedule->command('get:cylancethreats')->twiceDaily(15, 17)->timezone('America/Chicago');          // runs twice daily at 03:00pm and 05:00pm
-        */
 
         //$schedule->command('get:insidehosttrafficsnapshots')->twiceDaily(8, 14)->timezone('America/Chicago');   // runs twice daily at 08:00am and 02:00pm
         //$schedule->command('get:outsidehosttrafficsnapshots')->twiceDaily(9, 15)->timezone('America/Chicago');  // runs twice daily at 09:00am and 03:00pm
 
-        //$schedule->command('get:spamemail')->twiceDaily(4, 16)->timezone('America/Chicago');                // runs twice daily at 04:00am and 04:00pm
+        $schedule->command('get:spamemail')->twiceDaily(4, 16)->timezone('America/Chicago');                // runs twice daily at 04:00am and 04:00pm
 
         /*
         * Commands run on an hourly basis
