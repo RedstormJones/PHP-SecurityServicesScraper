87660f47daed8ae84ee8464bcad6d8ea6bd453a4
RedstormJones <tfilkins@unomaha.edu>
2017-07-05 13:25:52 +0000

    disabled command for SAP Role Auth incidents

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -133,7 +133,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('get:idmtasks')->weekdays()->hourly()->between('06:00', '18:00')->timezone('America/Chicago');                  // runs hourly on week days between 06:00am and 06:00pm
         //$schedule->command('get:saproleauthtasks')->weekdays()->hourly()->between('06:00', '18:00')->timezone('America/Chicago');          // runs hourly on week days between 06:00am and 06:00pm
 
-        $schedule->command('get:saproleauthincidents')->weekdays()->hourly()->between('06:00', '18:00')->timezone('America/Chicago');      // runs hourly on week days between 06:00am and 06:00pm
+        //$schedule->command('get:saproleauthincidents')->weekdays()->hourly()->between('06:00', '18:00')->timezone('America/Chicago');      // runs hourly on week days between 06:00am and 06:00pm
         $schedule->command('get:idmincidents')->weekdays()->hourly()->between('06:00', '18:00')->timezone('America/Chicago');              // runs hourly on week days between 06:00am and 06:00pm
         $schedule->command('get:securityincidents')->weekdays()->hourly()->between('06:00', '18:00')->timezone('America/Chicago');         // runs hourly on week days between 06:00am and 06:00pm
 
