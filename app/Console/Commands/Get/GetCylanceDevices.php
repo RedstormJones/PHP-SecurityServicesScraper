828912406336d22e0f553aeec6ef00ee70f3d34f
RedstormJones <redstormjones@gmail.com>
2017-09-12 18:19:52 +0000

    Apply fixes from StyleCI

--- app/Console/Commands/Get/GetCylanceDevices.php
+++ app/Console/Commands/Get/GetCylanceDevices.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands\Get;
 require_once app_path('Console/Crawler/Crawler.php');
 
 use App\Cylance\CylanceDevice;
-use App\Jobs\SendCylanceDevice;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
