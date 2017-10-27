ebe209822ea2a403e1c36fddbf2fbceb5892f6ca
RedstormJones <redstormjones@gmail.com>
2017-10-27 14:07:21 +0000

    fixed exception handling in GetProofPointSIEM command

--- app/Console/Commands/Get/GetProofPointSIEM.php
+++ app/Console/Commands/Get/GetProofPointSIEM.php
@@ -47,29 +47,46 @@ class GetProofPointSIEM extends Command
 
         Log::info(PHP_EOL.PHP_EOL.'*************************************'.PHP_EOL.'* Starting ProofPoint SIEM command! *'.PHP_EOL.'*************************************');
 
+        // setup cookie file and instantiate crawler
         $cookiejar = storage_path('app/cookies/proofpointcookie.txt');
         $crawler = new \Crawler\Crawler($cookiejar);
 
+        // setup auth header and assign to crawler
         $header = [
             'Authorization: Basic '.getenv('PROOFPOINT_AUTH'),
         ];
         curl_setopt($crawler->curl, CURLOPT_HTTPHEADER, $header);
 
+        // define target url
         $url = 'https://tap-api-v2.proofpoint.com/v2/siem/all?format=json&sinceSeconds=60';
 
+        // send GET request to url and dump response to file
         $json_response = $crawler->get($url);
         file_put_contents(storage_path('app/responses/proofpoint.response'), $json_response);
 
-        if (\Metaclassing\Utility::isJson($json_response)) {
+        // try to JSON decode the response
+        try {
             $response = \Metaclassing\Utility::decodeJson($json_response);
+            $error = "No errors detected";
+        }
+        catch (\Exception $e) {
+            $error = $e->getMessage();
+        }
+
+        // check if decoding the JSON response was successful
+        if (!\Metaclassing\Utility::testJsonError()) {
+
             file_put_contents(storage_path('app/collections/proofpoint_siem.json'), \Metaclassing\Utility::encodeJson($response));
 
+            // setup a Kafka producer
             $config = \Kafka\ProducerConfig::getInstance();
             $config->setMetadataBrokerList(getenv('KAFKA_BROKERS'));
             $producer = new \Kafka\Producer();
 
+            // add the current time to the data
             $response['UpsertDate'] = Carbon::now()->toAtomString();
 
+            // send data to Kafka
             $result = $producer->send([
                 [
                     'topic' => 'proofpoint_siem',
@@ -77,16 +94,18 @@ class GetProofPointSIEM extends Command
                 ],
             ]);
 
+            // check for errors
             if ($result[0]['data'][0]['partitions'][0]['errorCode']) {
                 Log::error('[!] Error sending to Kafka: '.$result[0]['data'][0]['partitions'][0]['errorCode']);
             } else {
                 Log::info('[*] ProofPoint SIEM data successfully sent to Kafka');
             }
         } else {
-            Log::error('[!] ProofPoint response not valid JSON: '.json_last_error());
-            die('[!] ERROR: ProofPoint response not valid JSON: '.json_last_error().PHP_EOL);
+            // otherwise pop smoke and bail
+            Log::error('[!] ProofPoint response not valid JSON: '.$error);
+            die('[!] ERROR: ProofPoint response not valid JSON: '.$error.PHP_EOL);
         }
 
-        Log::info('* ProofPoint SIEM command completed! *'.PHP_EOL);
+        Log::info('[*] ProofPoint SIEM command completed! [*]'.PHP_EOL);
     }
 }
