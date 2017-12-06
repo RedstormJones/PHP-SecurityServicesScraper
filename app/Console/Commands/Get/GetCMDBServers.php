505ce2e89f17c2d0a25207caf24cfa6325f8e62d
RedstormJones <redstormjones@gmail.com>
2017-12-06 09:30:16 +0000

    added upsert_date to CMDB servers before shipping to kafka

--- app/Console/Commands/Get/GetCMDBServers.php
+++ app/Console/Commands/Get/GetCMDBServers.php
@@ -285,6 +285,8 @@ class GetCMDBServers extends Command
         $producer = new \Kafka\Producer();
 
         foreach ($cmdb_servers as $server) {
+            $server['upsert_date'] = Carbon::now()->toAtomString();
+            
             $result = $producer->send([
                 [
                     'topic' => 'servicenow_cmdb_servers',
@@ -333,116 +335,6 @@ class GetCMDBServers extends Command
         }
         */
 
-        /*************************************
-         * [2] Process servers into database *
-         *************************************/
-        /*
-        Log::info(PHP_EOL.'*************************************'.PHP_EOL.'* Starting CMDB servers processing! *'.PHP_EOL.'*************************************');
-
-        foreach ($cmdb_servers as $server) {
-            $exists = cmdbServer::where('sys_id', $server['sys_id'])->value('id');
-
-            if ($exists) {
-                $servermodel = cmdbServer::find($exists);
-
-                $servermodel->update([
-                    'updated_on'            => $server['sys_updated_on'],
-                    'updated_by'            => $server['sys_updated_by'],
-                    'modified_count'        => $server['sys_mod_count'],
-                    'short_description'     => $server['short_description'],
-                    'ip_address'            => $server['ip_address'],
-                    'remote_mgmt_ip'        => $server['u_remote_mgmt_ip'],
-                    'application'           => $server['u_application'],
-                    'environment'           => $server['u_environment'],
-                    'data_center'           => $server['u_data_center'],
-                    'site_id'               => $server['u_siteid'],
-                    'business_process'      => $server['u_business_process'],
-                    'function'              => $server['u_function'],
-                    'notes'                 => $server['u_notes'],
-                    'product'               => $server['u_product'],
-                    'product_group'         => $server['u_product_group'],
-                    'antivirus_exclusions'  => $server['u_antivirus_exclusions'],
-                    'ktg_contact'           => $server['u_ktg_contact'],
-                    'virtual'               => $server['virtual'],
-                    'used_for'              => $server['used_for'],
-                    'firewall_status'       => $server['firewall_status'],
-                    'os'                    => $server['os'],
-                    'os_service_pack'       => $server['os_service_pack'],
-                    'os_version'            => $server['os_version'],
-                    'disk_space'            => $server['disk_space'],
-                    'operational_status'    => $server['operational_status'],
-                    'bpo'                   => $server['u_bpo'],
-                    'assigned_to'           => $server['assigned_to'],
-                    'district'              => $server['u_district'],
-                    'managed_by'            => $server['managed_by'],
-                    'owned_by'              => $server['owned_by'],
-                    'supported_by'          => $server['supported_by'],
-                    'support_group'         => $server['support_group'],
-                    'location'              => $server['location'],
-                    'data'                  => \Metaclassing\Utility::encodeJson($server),
-                ]);
-
-                $servermodel->save();
-
-                // touch server model to update 'updated_at' timestamp in case nothing was changed
-                $servermodel->touch();
-
-                Log::info('updated server: '.$server['name']);
-            } else {
-                Log::info('creating new server record: '.$server['name']);
-
-                $new_server = new cmdbServer();
-
-                $new_server->sys_id = $server['sys_id'];
-                $new_server->name = $server['name'];
-                $new_server->created_on = $server['sys_created_on'];
-                $new_server->updated_on = $server['sys_updated_on'];
-                $new_server->created_by = $server['sys_created_by'];
-                $new_server->updated_by = $server['sys_updated_by'];
-                $new_server->classification = $server['classification'];
-                $new_server->modified_count = $server['sys_mod_count'];
-                $new_server->short_description = $server['short_description'];
-                $new_server->os_domain = $server['os_domain'];
-                $new_server->ip_address = $server['ip_address'];
-                $new_server->remote_mgmt_ip = $server['u_remote_mgmt_ip'];
-                $new_server->application = $server['u_application'];
-                $new_server->environment = $server['u_environment'];
-                $new_server->data_center = $server['u_data_center'];
-                $new_server->site_id = $server['u_siteid'];
-                $new_server->business_process = $server['u_business_process'];
-                $new_server->business_function = $server['u_function'];
-                $new_server->notes = $server['u_notes'];
-                $new_server->product = $server['u_product'];
-                $new_server->product_group = $server['u_product_group'];
-                $new_server->antivirus_exclusions = $server['u_antivirus_exclusions'];
-                $new_server->ktg_contact = $server['u_ktg_contact'];
-                $new_server->virtual = $server['virtual'];
-                $new_server->used_for = $server['used_for'];
-                $new_server->firewall_status = $server['firewall_status'];
-                $new_server->os = $server['os'];
-                $new_server->os_service_pack = $server['os_service_pack'];
-                $new_server->os_version = $server['os_version'];
-                $new_server->disk_space = $server['disk_space'];
-                $new_server->operational_status = $server['operational_status'];
-                $new_server->model_number = $server['model_number'];
-                $new_server->serial_number = $server['serial_number'];
-                $new_server->managed_by = $server['managed_by'];
-                $new_server->owned_by = $server['owned_by'];
-                $new_server->supported_by = $server['supported_by'];
-                $new_server->support_group = $server['support_group'];
-                $new_server->location = $server['location'];
-                $new_server->bpo = $server['u_bpo'];
-                $new_server->assigned_to = $server['assigned_to'];
-                $new_server->district = $server['u_district'];
-                $new_server->data = \Metaclassing\Utility::encodeJson($server);
-
-                $new_server->save();
-            }
-        }
-
-        $this->processDeletes();
-        */
-
         Log::info('* CMDB servers completed! *'.PHP_EOL);
     }
 
@@ -475,23 +367,6 @@ class GetCMDBServers extends Command
             $some_data['display_value'] = null;
 
             return $some_data;
-        }
-    }
-
-    /**
-     * Delete old CylanceDevice models.
-     *
-     * @return void
-     */
-    public function processDeletes()
-    {
-        $delete_date = Carbon::now()->subDays(1);
-
-        $servers = cmdbServer::where('updated_at', '<', $delete_date)->get();
-
-        foreach ($servers as $server) {
-            Log::info('deleting server: '.$server->name);
-            $server->delete();
         }
     }
 }
