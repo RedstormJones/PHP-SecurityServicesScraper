43d51359326c6e3a87b82786d4d3402fb66327cc
RedstormJones <redstormjones@gmail.com>
2018-02-25 23:26:08 +0000

    eventually I'll get this right

--- app/Console/Commands/Get/GetCASAlertsLow.php
+++ app/Console/Commands/Get/GetCASAlertsLow.php
@@ -95,7 +95,7 @@ class GetCASAlertsLow extends Command
                 $count += count($low_alerts);
             } else {
                 // if no then M$ is probably throttling us, so sleep it off
-                Log::warning('[WARN] no data found in response: '.data);
+                Log::warning('[WARN] no data found in response: '.$data);
                 Log::info('[+] sleeping it off (5 sec)...');
                 sleep(5);
             }
