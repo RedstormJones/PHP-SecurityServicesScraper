00a3e7b53ab6852c3961a0e4093101d91c2819b7
Tyler.Filkins <tyler.filkins@kiewit.com>
2020-07-02 13:35:17 -0500

    forgot to include Carbon

--- app/Console/Commands/Get/GetProofPointSIEM.php
+++ app/Console/Commands/Get/GetProofPointSIEM.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands\Get;
 
 require_once app_path('Console/Crawler/Crawler.php');
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
