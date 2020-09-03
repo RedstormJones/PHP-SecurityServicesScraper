3993a81e57de3c356fb87a27c90903d89c9eed86
Tyler.Filkins <tyler.filkins@kiewit.com>
2020-09-03 13:24:14 -0500

    created command to pull data from PhishLabs Case API

---
+++ app/Console/Commands/Get/GetPhishLabsIncidents.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Console\Commands\Get;
+
+require_once app_path('Console/Crawler/Crawler.php');
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class GetPhishLabsIncidents extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get:phishlabsincidents';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get incidents from the PhishLabs Case API';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info('[GetPhishLabsIncidents.php] Starting API Poll!');
+
+        $date = Carbon::now()->subHour();
+        $date_str = $date->toIso8601ZuluString();
+
+        $output_date = Carbon::now()->toDateString();
+
+        // setup cookie jar
+        $cookiejar = storage_path('app/cookies/phishlabs_case_api.txt');
+
+        // setup crawler
+        $crawler = new \Crawler\Crawler($cookiejar);
+
+        // get auth string from secrets file
+        $phishlabs_auth = getenv('PHISHLABS_AUTH');
+
+        // build auth header and add to crawler
+        $headers = [
+            'Authorization: Basic '.$phishlabs_auth,
+        ];
+        curl_setopt($crawler->curl, CURLOPT_HTTPHEADER, $headers);
+
+        // setup url params and convert to &-delimited string
+        $url_params = [
+            'created_after' => $date_str,
+            'direction'     => 'asc',
+            'sort'          => 'created_after',
+        ];
+        $url_params_str = $this->postArrayToString($url_params);
+
+        // send request, capture response and dump to file
+        $json_response = $crawler->get(getenv('PHISHLABS_CASE_URL').'/v1/incidents/EIR?'.$url_params_str);
+        file_put_contents(storage_path('app/responses/phishlabs-incidents.json'), $json_response);
+
+        // attempt to JSON decode the response
+        try {
+            $response = \Metaclassing\Utility::decodeJson($json_response);
+        } catch (\Exception $e) {
+            Log::error('[GetPhishLabsIncidents.php] attempt to decode JSON response failed: '.$e->getMessage());
+            die('[GetPhishLabsIncidents.php] attempt to decode JSON response failed: '.$e->getMessage());
+        }
+
+        // check for errors or unknown results in the response
+        if (array_key_exists('error', $response)) {
+            Log::error('[GetPhishLabsIncidents.php] error in response: '.$json_response);
+            die('[GetPhishLabsIncidents.php] error in response: '.$json_response.PHP_EOL);
+        } elseif (array_key_exists('incidents', $response)) {
+            $incidents = $response['incidents'];
+
+            $incidents_count = $response['metadata']['count'];
+            Log::info('[GetPhishLabsIncidents.php] count of incidents from last hour: '.$incidents_count);
+        } else {
+            Log::error('[GetPhishLabsIncidents.php] unidentified response: '.$json_response);
+            die('[GetPhishLabsIncidents.php] unidentified response: '.$json_response);
+        }
+
+        file_put_contents(storage_path('app/collections/phishlabs-incidents.json'), \Metaclassing\Utility::encodeJson($incidents));
+
+        foreach ($incidents as $data) {
+            $data_json = \Metaclassing\Utility::encodeJson($data)."\n";
+            file_put_contents(storage_path('app/output/phishlabs_incidents/'.$output_date.'-phishlabs-incidents.log'), $data_json, FILE_APPEND);
+        }
+
+        Log::info('[GetPhishLabsIncidents.php] DONE!');
+    }
+
+
+    /**
+     * Function to convert post information from an assoc array to a string.
+     *
+     * @return string
+     */
+    public function postArrayToString($post)
+    {
+        $postarray = [];
+        foreach ($post as $key => $value) {
+            $postarray[] = $key.'='.$value;
+        }
+
+        // takes the postarray array and concatenates together the values with &'s
+        $poststring = implode('&', $postarray);
+
+        return $poststring;
+    }
+
+}
