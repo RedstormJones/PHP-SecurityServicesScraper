cc8e9c97cf873bf6122823e6c4f1a2d5090bdb8e
Tyler.Filkins <tyler.filkins@kiewit.com>
2020-10-09 09:11:22 -0500

    updates to GetPhishLabsNewDomains command

--- app/Console/Commands/Get/GetPhishLabsNewDomains.php
+++ app/Console/Commands/Get/GetPhishLabsNewDomains.php
@@ -44,7 +44,7 @@ class GetPhishLabsNewDomains extends Command
         Log::info('[GetPhishLabsNewDomains.php] Starting PhishLabs New Domains API Poll!');
 
         // calculate time ranges for URL parameters
-        $sub_hours = 4;
+        $sub_hours = 1;
         $from_date = Carbon::now()->subHours($sub_hours)->setTimezone('America/Chicago');
         $to_date = Carbon::now()->setTimezone('America/Chicago')->toDateTimeString();
         $from_date_short = substr($from_date->toDateTimeString(), 0, -3);
