4585787efbbf1f7889adfdfe024124394807f028
Tyler.Filkins <tyler.filkins@kiewit.com>
2020-10-27 10:10:28 -0500

    disabled GetGraphSecurityAlerts and GetNexposeSites commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,8 +39,8 @@ class Kernel extends ConsoleKernel
         // Commands run on an hourly basis
         $schedule->command('get:defenderatpalerts')->hourly()->timezone('America/Chicago');                 // runs hourly
         $schedule->command('get:newdomains')->hourly()->timezone('America/New_York');                       // runs hourly
-        $schedule->command('get:nexposesites')->hourly()->timezone('America/Chicago');                      // runs hourly
-        $schedule->command('get:graphsecurityalerts')->hourly()->timezone('America/Chicago');               // runs hourly
+        //$schedule->command('get:nexposesites')->hourly()->timezone('America/Chicago');                      // runs hourly
+        //$schedule->command('get:graphsecurityalerts')->hourly()->timezone('America/Chicago');               // runs hourly
         $schedule->command('get:threatindicators')->hourly()->timezone('America/Chicago');                  // runs hourly
         $schedule->command('get:phishlabsincidents')->hourly()->timezone('America/Chicago');                // runs hourly
         
