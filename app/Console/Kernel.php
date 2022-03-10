01fd57f89fa17f228072203a2bda666b55759ae1
Tyler.Filkins <tyler.filkins@kiewit.com>
2022-03-10 14:41:36 -0600

    rescheduled GetTrapIncidents command to run every ten minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,9 +30,9 @@ class Kernel extends ConsoleKernel
         // Commands run on an hourly basis
         $schedule->command('get:defenderatpalerts')->hourly()->timezone('America/Chicago');
         $schedule->command('get:newdomains')->hourly()->timezone('America/New_York');
-        $schedule->command('get:trapincidents')->hourly()->timezone('America/Chicago');
 
         // Commands run every ten minutes
+        $schedule->command('get:trapincidents')->everyTenMinutes()->timezone('America/Chicago');
         $schedule->command('get:threatindicators')->everyTenMinutes()->timezone('America/Chicago');
         $schedule->command('get:phishlabsincidents')->everyTenMinutes()->timezone('America/Chicago');
         $schedule->command('get:lastpassevents')->everyTenMinutes()->timezone('America/Chicago');
