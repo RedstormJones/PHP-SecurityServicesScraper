aa7f52bddd30f9dd6b582a0b9e05581532f3062d
Tyler.Filkins <tyler.filkins@kiewit.com>
2022-03-23 10:47:30 -0500

    scheduled GetDefenderForEndpointIncidents command to run every 10 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('get:newdomains')->hourly()->timezone('America/New_York');
 
         // Commands run every ten minutes
+        $schedule->command('get:endpointincidents')->everyTenMinutes()->timezone('America/Chicago');
         $schedule->command('get:trapincidents')->everyTenMinutes()->timezone('America/Chicago');
         $schedule->command('get:threatindicators')->everyTenMinutes()->timezone('America/Chicago');
         $schedule->command('get:phishlabsincidents')->everyTenMinutes()->timezone('America/Chicago');
